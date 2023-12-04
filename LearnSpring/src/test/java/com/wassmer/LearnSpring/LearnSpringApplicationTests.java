5735148cb1ddfb3e70b501dbd06b6ba08947f096
Marcel <marcel.wassmer@gmail.com>
2023-12-04 22:59:49 +0800

    Initial commit

---
+++ LearnSpring/src/test/java/com/wassmer/LearnSpring/LearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wassmer.LearnSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
